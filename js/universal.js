41b04e0fd79c1a81dd00be9e48b7c4526261dd9f
Mehul Smriti Raje <mehul.raje@gmail.com>
2015-06-09 02:32:20 +0530

    Changes to design and layout

--- js/universal.js
+++ js/universal.js
@@ -1,72 +1,73 @@
+var state=true;
+
 $('#home-link').on('click',function(){
-  $('#home').show();
-  $('#proj').hide();
-  $('#pub').hide();
-  $('#pos').hide();
-  $('#wom').hide();
-  $('#blog').hide();
-  $('#con').hide();
+  $('#home').show(); $('#home-link').css("color","#FFA000");
+  $('#proj').hide(); $('#proj-link').css("color","#000000");
+  $('#pub').hide(); $('#oub-link').css("color","#000000");
+  $('#pos').hide(); $('#pos-link').css("color","#000000");
+  $('#wom').hide(); $('#wom-link').css("color","#000000");
+  $('#blog').hide(); $('#blog-link').css("color","#000000");
+  $('#con').hide(); $('#con-link').css("color","#000000");
 });
 
 $('#proj-link').on('click',function(){
-  $('#home').hide();
-  $('#proj').show();
-  $('#pub').hide();
-  $('#pos').hide();
-  $('#wom').hide();
-  $('#blog').hide();
-  $('#con').hide();
+  $('#home').hide(); $('#home-link').css("color","#000000");
+  $('#proj').show(); $('#proj-link').css("color","#FFA000");
+  $('#pub').hide(); $('#pub-link').css("color","#000000");
+  $('#pos').hide(); $('#pos-link').css("color","#000000");
+  $('#wom').hide(); $('#wom-link').css("color","#000000");
+  $('#blog').hide(); $('#blog-link').css("color","#000000");
+  $('#con').hide(); $('#con-link').css("color","#000000");
 });
 
 $('#pub-link').on('click',function(){
-  $('#home').hide();
-  $('#proj').hide();
-  $('#pub').show();
-  $('#pos').hide();
-  $('#wom').hide();
-  $('#blog').hide();
-  $('#con').hide();
+  $('#home').hide(); $('#home-link').css("color","#000000");
+  $('#proj').hide(); $('#proj-link').css("color","#000000");
+  $('#pub').show(); $('#pub-link').css("color","#FFA000");
+  $('#pos').hide(); $('#pos-link').css("color","#000000");
+  $('#wom').hide(); $('#wom-link').css("color","#000000");
+  $('#blog').hide(); $('#blog-link').css("color","#000000");
+  $('#con').hide(); $('#con-link').css("color","#000000");
 });
 
 $('#pos-link').on('click',function(){
-  $('#home').hide();
-  $('#proj').hide();
-  $('#pub').hide();
-  $('#pos').show();
-  $('#wom').hide();
-  $('#blog').hide();
-  $('#con').hide();
+  $('#home').hide(); $('#home-link').css("color","#000000");
+  $('#proj').hide(); $('#proj-link').css("color","#000000");
+  $('#pub').hide(); $('#pub-link').css("color","#000000");
+  $('#pos').show(); $('#pos-link').css("color","#FFA000");
+  $('#wom').hide(); $('#wom-link').css("color","#000000");
+  $('#blog').hide(); $('#blog-link').css("color","#000000");
+  $('#con').hide(); $('#con-link').css("color","#000000");
 });
 
 $('#wom-link').on('click',function(){
-  $('#home').hide();
-  $('#proj').hide();
-  $('#pub').hide();
-  $('#pos').hide();
-  $('#wom').show();
-  $('#blog').hide();
-  $('#con').hide();
+  $('#home').hide(); $('#home-link').css("color","#000000");
+  $('#proj').hide(); $('#proj-link').css("color","#000000");
+  $('#pub').hide(); $('#pub-link').css("color","#000000");
+  $('#pos').hide(); $('#pos-link').css("color","#000000");
+  $('#wom').show(); $('#wom-link').css("color","#FFA000");
+  $('#blog').hide(); $('#blog-link').css("color","#000000");
+  $('#con').hide(); $('#con-link').css("color","#000000");
 });
 
 $('#blog-link').on('click',function(){
-  $('#home').hide();
-  $('#proj').hide();
-  $('#pub').hide();
-  $('#pos').hide();
-  $('#wom').hide();
-  $('#blog').show();
-  $('#con').hide();
+  $('#home').hide(); $('#home-link').css("color","#000000");
+  $('#proj').hide(); $('#proj-link').css("color","#000000");
+  $('#pub').hide(); $('#pub-link').css("color","#000000");
+  $('#pos').hide(); $('#pos-link').css("color","#000000");
+  $('#wom').hide(); $('#wom-link').css("color","#000000");
+  $('#blog').show(); $('#blog-link').css("color","#FFA000");
+  $('#con').hide(); $('#con-link').css("color","#000000");
 });
 
 $('#con-link').on('click',function(){
-  $('#home').hide();
-  $('#proj').hide();
-  $('#pub').hide();
-  $('#pos').hide();
-  $('#wom').hide();
-  $('#blog').hide();
-  $('#con').show();
-  console.log("Contact pressed");
+  $('#home').hide(); $('#home-link').css("color","#000000");
+  $('#proj').hide(); $('#proj-link').css("color","#000000");
+  $('#pub').hide(); $('#pub-link').css("color","#000000");
+  $('#pos').hide(); $('#pos-link').css("color","#000000");
+  $('#wom').hide(); $('#wom-link').css("color","#000000");
+  $('#blog').hide(); $('#blog-link').css("color","#000000");
+  $('#con').show(); $('#con-link').css("color","#FFA000");
 });
 
 // $('#submit').click(function() {
@@ -77,6 +78,16 @@ $('#con-link').on('click',function(){
 // });
 
 $('#submit').click(function() {
-
-  $("#footer").get(0).scrollIntoView();
+  if(state)
+  {
+    state=false;
+    $(".container").get(0).scrollIntoView();
+    $('#submit').css("transform", "rotate(180deg)",100);
+  }
+  else
+  {
+    state=true;
+    $("#front").get(0).scrollIntoView();
+    $('#submit').css("transform", "rotate(360deg)");
+  }
 });
